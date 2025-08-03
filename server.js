6101d350ec152ec59681a2d913569d45c9e1781c
sunnylenz <ikesundayshield@gmail.com>
2025-08-03 14:27:48 +0100

    Error handler for ROUTE NOT FOUND

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 require("./config/dbconnection");
 const app = express();
 
-
-
 //middlewares
 // app.use(cors())
 // app.use(cookieParser())
@@ -13,7 +11,12 @@ app.use(express.json());
 //routes
 //error handlers
 app.use(globalErrHandler);
-
+//404 error
+app.use('*', (req, res) => {
+    res.status(404).json({
+        message: `${req.originalUrl} - Route Not Found`,
+    });
+});
 
 //listen to the server
 const PORT = process.env.PORT || 9000;
